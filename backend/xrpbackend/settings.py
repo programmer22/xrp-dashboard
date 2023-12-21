6542f4e6305a1395e391b54d75092ac6d988ee78
Nick Lopacki <nlopacki@psnamericas.com>
2023-12-20 19:37:42 -0600

    Configure STATIC_ROOT for Heroku deployment

--- backend/xrpbackend/settings.py
+++ backend/xrpbackend/settings.py
@@ -140,6 +140,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
